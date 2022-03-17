93ab8f65e5cd7b3bdd16fe4c8a28620f9310b560
Freanu Peria <magnussonfrancis@gmail.com>
2022-03-17 16:41:13 +0800

    can now show list.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/hero';
 
+import { HEROES } from 'src/app/mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,10 +14,16 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: "Windstorm"
   }
+  selectedHero?: Hero;
+  heroes: Hero[] = HEROES;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
